26363ef976091aff129f640d69f63b97c8bd7ca7
Nathan Hall <nathan@whatshark.com>
2022-10-07 21:50:25 +0100

    Add Material UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,21 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import CssBaseline from "@mui/material/CssBaseline";
+import Theme from "../src/theme";
+import { ThemeProvider } from "@mui/material/styles";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <CssBaseline />
+      <ThemeProvider theme={Theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,34 @@
-import type { NextPage } from 'next'
-import profilePic from '../public/shark.png'
-import Link from 'next/link'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import profilePic from "../public/shark.png";
+import Link from "next/link";
+import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
     <div>
       <p>WhatShark</p>
       <ul>
-        <li>Contact me: <Link href="mailto:nathan@whatshark.com">nathan@whatshark.com</Link></li>
-        <li>GitHub repos: <Link href="https://github.com/WhatShark/">https://github.com/WhatShark/</Link></li>
-        <li>Public key: <Link href="/public-key.pub">C956 5B05 9C11 FC53</Link></li>
+        <li>
+          Contact me:{" "}
+          <Link href="mailto:nathan@whatshark.com">nathan@whatshark.com</Link>
+        </li>
+        <li>
+          GitHub repos:{" "}
+          <Link href="https://github.com/WhatShark/">
+            https://github.com/WhatShark/
+          </Link>
+        </li>
+        <li>
+          Public key: <Link href="/public-key.pub">C956 5B05 9C11 FC53</Link>
+        </li>
       </ul>
 
-      <Image src={profilePic} alt="Picture of a great white shark. Distorted." />
+      <Image
+        src={profilePic}
+        alt="Picture of a great white shark. Distorted."
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
