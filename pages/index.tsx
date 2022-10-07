78142a32328a177e6f1a6bb1c0f681ce893eea7f
Nathan Hall <nathan@whatshark.com>
2022-10-07 21:19:32 +0100

    Essential details & AWS Amplify image test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,21 @@
 import type { NextPage } from 'next'
+import profilePic from '../public/shark.png'
+import Link from 'next/link'
+import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <p>WhatShark Application</p>
+    <div>
+      <p>WhatShark</p>
+      <ul>
+        <li>Contact me: <Link href="mailto:nathan@whatshark.com">nathan@whatshark.com</Link></li>
+        <li>GitHub repos: <Link href="https://github.com/WhatShark/">https://github.com/WhatShark/</Link></li>
+        <li>Public key: <Link href="/public-key.pub">C956 5B05 9C11 FC53</Link></li>
+      </ul>
+
+      <img alt="Picture of a great white shark. Distorted." src="/shark.png"></img>
+      <Image src={profilePic} alt="Picture of a great white shark. Distorted." />
+    </div>
   )
 }
 
